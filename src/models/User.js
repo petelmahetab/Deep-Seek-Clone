8491ebe5c4818880de431963cda4b0485d2a0c3a
Mahetab Patel <patelmahetab9020@gmail.com>
2025-05-19 20:37:33 +0530

    Installed regenerate unicode properties

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
 
 const UserSchema = new mongoose.Schema(
-    {
-        _id: { type: String, required: true },
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        image: { type: String, required: false }
-    }, {
-    timestamps: true
-}
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User=mongoose.models.User || mongoose.model('User',UserSchema);
-
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 export default User;
